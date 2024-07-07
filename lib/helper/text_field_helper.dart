47c6d946f90bb4bb8da057da7fb453be18304d90
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-07-07 22:12:26 +0300

    Some Refactors in text_field_helper File and custom_text_field File for Clean Code

---
+++ lib/helper/text_field_helper.dart
@@ -0,0 +1,26 @@
+  import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+OutlineInputBorder outlineInputBorder([Color? color]) {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: color ?? Colors.white,
+      ),
+      borderRadius: const BorderRadius.all(Radius.circular(8)),
+    );
+  }
+
+InputDecoration inputDecoration([String? hintText, double? contentPadding]) {
+    return InputDecoration(
+      fillColor: Colors.white,
+      border: outlineInputBorder(),
+      enabledBorder: outlineInputBorder(),
+      focusedBorder: outlineInputBorder(kPrimaryColor),
+      hintText: hintText,
+      hintStyle: const TextStyle(color: kPrimaryColor),
+      contentPadding: EdgeInsets.symmetric(
+        horizontal: 12,
+        vertical: contentPadding ?? 16,
+      ),
+    );
+  }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
-import 'package:notes_app/helper/text_field_border_helper.dart';
+import 'package:notes_app/helper/text_field_helper.dart';
 
-// ignore: must_be_immutable
 class CustomTextField extends StatelessWidget {
-  CustomTextField(
-      {super.key,
-      required this.hintText,
-      this.contentPadding});
-  String hintText;
-  double? contentPadding;
-  Function(String)? onChanged;
+  const CustomTextField(
+      {super.key, required this.hintText, this.contentPadding});
+  final String hintText;
+  final double? contentPadding;
   @override
   Widget build(BuildContext context) {
     return TextField(
-      scrollPhysics: const BouncingScrollPhysics(),
-      onChanged: onChanged,
-      style: TextStyle(
-        color: Colors.grey.shade100,
-      ),
-      decoration: InputDecoration(
-        fillColor: Colors.white,
-        border: outlineInputBorder(),
-        enabledBorder: outlineInputBorder(),
-        focusedBorder: outlineInputBorder(kPrimaryColor),
-        hintText: hintText,
-        hintStyle: const TextStyle(color: kPrimaryColor),
-        contentPadding: EdgeInsets.symmetric(
-          horizontal: 12,
-          vertical: contentPadding ?? 16,
-        ),
-      ),
+      style: TextStyle(color: Colors.grey.shade100),
+      decoration: inputDecoration(hintText, contentPadding),
       cursorColor: kPrimaryColor,
     );
   }
