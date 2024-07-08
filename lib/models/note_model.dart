103b28ce647adc30e068eaf05bcb6649a3f60e84
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-07-08 23:24:42 +0300

    Creating Note Model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,22 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final DateTime date;
+  final int color;
+
+  NoteModel({
+    required this.color,
+    required this.title,
+    required this.subTitle,
+    required this.date,
+  });
+
+  factory NoteModel.fromJson(Map<String, dynamic> json) {
+    return NoteModel(
+      color: json['color'],
+      title: json['title'],
+      subTitle: json['subTitle'],
+      date: DateTime.parse(json['date']),
+    );
+  }
+}
