642b85a79da59f321ec369d380507c8d9f1b276f
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-07-12 00:57:42 +0300

    Add note state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,6 +1,16 @@
 part of 'add_note_cubit.dart';
 
 @immutable
-sealed class AddNoteState {}
+abstract class AddNoteState {}
 
-final class AddNoteInitial extends AddNoteState {}
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errorMessage;
+
+  AddNoteFailure(this.errorMessage);
+}
