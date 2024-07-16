25ee4c30e67062445655a7ae150d5b0ed357f1c9
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-07-16 23:07:12 +0300

    Create Get Notes Cubit

--- lib/cubits/get_notes_cubit/get_notes_cubit.dart
+++ lib/cubits/get_notes_cubit/get_notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'get_notes_state.dart';
 
 class GetNotesCubit extends Cubit<GetNotesState> {
   GetNotesCubit() : super(GetCubitInitial());
+
+  fetchAllNotes() {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(GetNoteSuccess(notes));
+    } catch (e) {
+      emit(GetNoteFailure(e.toString()));
+    }
+  }
 }
