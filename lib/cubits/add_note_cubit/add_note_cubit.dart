e4161711a3d5919a40f0f9f922f30f63bf9dd221
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-07-20 19:09:50 +0300

    Add Note Color using Flex Color Package

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
@@ -8,7 +9,9 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = Colors.grey.shade200;
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,6 +1,8 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_button.dart';
@@ -19,6 +21,28 @@ class _AddNoteFormState extends State<AddNoteForm> {
   final GlobalKey<FormState> formKey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   String? title, subTitle;
+  late Color selectedColor;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = BlocProvider.of<AddNoteCubit>(context).color;
+  }
+
+  Future<void> showPicker() {
+    return ColorPicker(
+      showColorName: true,
+      color: selectedColor,
+      onColorChanged: (Color color) {
+        setState(() {
+          selectedColor = color;
+        });
+      },
+    ).showPickerDialog(context).then((_) {
+      BlocProvider.of<AddNoteCubit>(context).color = selectedColor;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -26,6 +50,31 @@ class _AddNoteFormState extends State<AddNoteForm> {
       autovalidateMode: autovalidateMode,
       child: Column(
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Pick A Color !',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: showPicker,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(kPrimaryColor),
+                ),
+                child: const Text(
+                  'Pick',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
           CustomTextField(
             hintText: 'Title',
             onSaved: (value) {
@@ -49,9 +98,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
-                    var currentDate = DateFormat.yMMMMd().format(DateTime.now());
+                    var currentDate =
+                        DateFormat.yMMMMd().format(DateTime.now());
                     var noteModel = NoteModel(
-                      color: Colors.blue.value,
+                      color: selectedColor.value,
                       date: currentDate,
                       subTitle: subTitle!,
                       title: title!,
